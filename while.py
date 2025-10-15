d5349512856fd68adf0f1b75ebfd226dae9c7ceb
Aminjon <realaminjon77@gmail.com>
2025-10-15 11:09:15 +0500

    Add files via upload

---
+++ while.py
@@ -0,0 +1,11 @@
+ps = '12345'
+a = True
+while a:
+    password = input('Parol Kirit')
+    if password==ps:
+        print("Xush kelibsan")
+        a = False
+    else:
+        a=False
+        print('Xato')
+       
